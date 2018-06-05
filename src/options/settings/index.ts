f7f56bce31297c2061e6581b11906b2d70e09825
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2018-06-05 10:48:22 +0700

    Fix typing issues from mixed up dep version locks

---
+++ src/options/settings/index.ts
@@ -0,0 +1,7 @@
+import Settings from './components/Settings'
+import * as actions from './actions'
+import * as selectors from './selectors'
+import reducer from './reducer'
+
+export { actions, reducer, selectors }
+export default Settings
